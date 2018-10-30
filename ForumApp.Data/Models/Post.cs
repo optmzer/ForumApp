b9203b532b26a3920ccb2034f3850dfc79107b29
Alexander Frolov <big_air2409@mail.ru>
2018-10-30 19:59:47 +1300

    Added Post class.

--- ForumApp.Data/Models/Post.cs
+++ ForumApp.Data/Models/Post.cs
@@ -6,5 +6,16 @@ namespace ForumApp.Data.Models
 {
     public class Post
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Created { get; set; }
+        //Later on can add upload for pictures
+
+        public virtual ApplicationUser User { get; set; }
+        public virtual Forum Forum { get; set; }
+
+        //Replies
+        public virtual IEnumerable<PostReply> Replies { get; set; }
     }
 }
