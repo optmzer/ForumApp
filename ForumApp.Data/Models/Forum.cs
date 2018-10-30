69a060cfda14106d6fdafbadfe712ec9a4a7ce37
Alexander Frolov <big_air2409@mail.ru>
2018-10-30 19:54:46 +1300

    Created Forum class.

--- ForumApp.Data/Models/Forum.cs
+++ ForumApp.Data/Models/Forum.cs
@@ -6,5 +6,15 @@ namespace ForumApp.Data.Models
 {
     public class Forum
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public string ImageUrl { get; set; }
+
+        //Collection of posts
+        public virtual IEnumerable<Post> Posts { get; set; }
+
+
     }
 }
