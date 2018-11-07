b6e469d30cad00c22981e484d2a2f141c768a07d
Alexander Frolov <big_air2409@mail.ru>
2018-11-07 18:53:35 +1300

    changed ApplicationUser for IdentityUser and DB migration is success. Works OK.

--- ForumApp.Data/Migrations/20181107054237_Changed ApplicationUser for IdentityUser.Designer.cs
+++ ForumApp.Data/Migrations/20181107054237_Changed ApplicationUser for IdentityUser.Designer.cs
@@ -4,14 +4,16 @@ using ForumApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ForumApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181107054237_Changed ApplicationUser for IdentityUser")]
+    partial class ChangedApplicationUserforIdentityUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
