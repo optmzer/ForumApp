9dff916cf82c81ebb82ea5e9481e5fc12ade425a
Alexander Frolov <big_air2409@mail.ru>
2018-12-08 18:55:30 +1300

    Added UserProfileModels.

---
+++ ForumApp/Models/ApplicationUser/UserProfileListModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+/**
+ * Wrapper for UserProfileModel 
+ */
+namespace ForumApp.Models.ApplicationUser
+{
+    public class UserProfileListModel
+    {
+    }
+}
---
+++ ForumApp/Models/ApplicationUser/UserProfileModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+
+namespace ForumApp.Models.ApplicationUser
+{
+    public class UserProfileModel
+    {
+        public string UserId { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string UserRating { get; set; }
+        public string UserProfileImageUrl { get; set; }
+
+        public DateTime UserProfileCreated { get; set; }
+        public IFormFile ImageUpload { get; set; }
+    }
+}
