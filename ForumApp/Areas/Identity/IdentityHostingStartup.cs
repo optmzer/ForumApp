32152282d7dca345db0e6dc41c74f00a68dbabfe
Alexander Frolov <big_air2409@mail.ru>
2018-12-12 16:07:01 +1300

    Overriden Login Input to display Username instead of email. Works OK

---
+++ ForumApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ForumApp.Data;
+using ForumApp.Data.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ForumApp.Areas.Identity.IdentityHostingStartup))]
+namespace ForumApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
